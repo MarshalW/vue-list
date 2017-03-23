5e1fb9d4720570346b31ea0d91f12112e43c3776
Marshal <marshal.wu@qq.com>
2017-03-23 21:33:11 +0800

    加载新闻完整功能

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
 <div id="app">
     <div>
-        <span>用户名：</span><input v-model="userName" /><button v-on:click="onSubmit">提交</button>
+      <h2>新闻列表</h2>
+      <button v-on:click="onReload">刷新</button>
+      <div>
+        <ul>
+            <li v-for="item in news">
+              {{item.title}}
+            </li>
+        </ul>
+      </div>
     </div>
-    <p>输入的内容是：{{userName}}</p>
 </div>
 </template>
 
@@ -14,16 +21,19 @@ export default {
     name: 'app',
     data: function() {
         return {
-            userName: ''
+            news:[]
         };
     },
     methods: {
-        onSubmit: function() {
+        onReload: function() {
             axios.get('http://7xr9y9.com1.z0.glb.clouddn.com/news-demo/news.json')
                 .then((response) => {
-                  alert(response.data.news[0].title);
+                  this.news=response.data.news;
                 })
         }
+    },
+    mounted:function(){
+        this.onReload();
     }
 }
 </script>
@@ -33,7 +43,6 @@ export default {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
     margin-top: 60px;
 }
@@ -41,23 +50,5 @@ export default {
 h1,
 h2 {
     font-weight: normal;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
-}
-
-button {
-    margin-left: 2px;
 }
 </style>
